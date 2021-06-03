61857b68dadb096d1b588fc5986b768a667bcc29
jsing <js449>
2021-06-03 15:28:16 +1200

    COMPX341-21A_LoginInputTestFail

---
+++ assets/pipeline.sh
@@ -0,0 +1,21 @@
+#Student Name & ID
+echo "Student Name: Jagdeep Singh"
+echo "Student ID: 1367758"
+
+# Continues Integration using shell commands
+echo "CI Process started"
+
+# OS information
+uname -a
+
+# Working dir
+echo "Project working directory"
+
+# Install all dpenedencies of node_modules
+# npm install
+
+# Running build
+-- --watchAll=false || npm run build
+
+# Running all the unittest cases
+npm run test 
---
+++ assets/src/modules/signup/Login.test.tsx
@@ -0,0 +1,45 @@
+/*
+* Student Name: Jagdeep Singh
+* Student ID: 1367758
+
+* All test cases for Login component
+*/
+
+import React from 'react';
+import Login from './Login';
+import ReactDOM from 'react-dom';
+
+
+describe('Login Component Test', () => {
+//Root container in which we will load our components
+  let container: HTMLDivElement
+
+  /**
+   * BEfore each loop which will set the stage for our testing
+   * Will run from every it statementand load the login component in it
+   */
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    ReactDOM.render(<Login />, container);
+  });
+
+  /**
+   * After each loop run after every it statement and cleanup our test best and destroy
+   * all the resources used by the test
+   * 
+   */
+  afterEach(() => {
+    document.body.removeChild(container);
+    container.remove();
+  });
+
+  //testing the numper of login inputs in login form
+  it('Number of inputs in form', () => {
+      //Selecting the container where all the inputs of the page available
+    const inputs = container.querySelectorAll('input');
+    //checking the length of the the inputs with expecting length.
+    expect(inputs).toHaveLength(5);
+  });
+
+});
